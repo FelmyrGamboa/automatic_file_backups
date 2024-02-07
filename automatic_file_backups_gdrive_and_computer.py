545b86beef5ca4157abf1df300d2aae822d40407
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-02-07 22:26:55 +0800

    Created the directories of the backup files and function to copy files to the directory

--- automatic_file_backups_gdrive_and_computer.py
+++ automatic_file_backups_gdrive_and_computer.py
@@ -15,3 +15,17 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
+
+source_dir = r"C:\Users\Felmyr Gamboa\Desktop\BSCpE 1-5 First Sem\Programming Logic and Design\Final Project\backupfiles_trial"
+destination_dir = r"C:\Users\Felmyr Gamboa\Desktop\Backups"
+
+def creating_backup_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    destination_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {destination_dir}")
+
+    except FileExistsError:
+        print(f"Folder already exists in: {dest} \n")
